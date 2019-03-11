57ea24bcb8fc283800fe0ed48806474183a8991f
Constantina Hug <constantina.hug@gmail.com>
2019-03-11 22:40:25 +0000

    added check for repeat letters and redirection for the end of a game

--- app.rb
+++ app.rb
@@ -40,6 +40,10 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    unique_guess = @game.guess(letter)
+    if unique_guess == false
+      flash[:message] = "You have already used that letter."
+    end
     redirect '/show'
   end
   
@@ -51,6 +55,11 @@ class HangpersonApp < Sinatra::Base
   get '/show' do
     ### YOUR CODE HERE ###
     erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      redirect '/lose'
+    elsif @game.check_win_or_lose == :win
+      redirect '/win'
+    end
   end
   
   get '/win' do
